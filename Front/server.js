12df4d226ef09056c018df6ab79ce2dcd33fce22
donourouh <bambahamed2001@gmail.com>
2025-04-21 20:09:31 +0000

    correction probleme front

--- Front/server.js
+++ Front/server.js
@@ -1,7 +1,7 @@
 // ICI UN SERVEUR NODEJS ARCHI MINIMAL
 // QUI VA SE CONTENTER DE RENVOYER LA PAGE
 // index.html située dans le dossier 
-// dist/assignment-app+/browser
+// dist/assignment-app/browser
 
 //Install express server
 const express = require("express");
@@ -10,11 +10,13 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/assignment-app/browser"));
+app.use(express.static(path.join(__dirname, "/dist/assignment-app/browser")));
 
 app.get("/*", function (req, res) {
- res.sendFile(path.join(__dirname + "/dist/assignment-app/browser/index.html"));
+  res.sendFile(path.join(__dirname, "/dist/assignment-app/browser/index.html"));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8081);
+// Start the app by listening on the default port
+app.listen(process.env.PORT || 8081, () => {
+  console.log(`Server is running on port ${process.env.PORT || 8081}`);
+});
