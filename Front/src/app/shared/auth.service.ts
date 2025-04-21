2333d6a9b73582267158c6440ae336c39a81b3bf
donourouh <bambahamed2001@gmail.com>
2025-04-21 21:41:18 +0000

    correction base de données api back a jour 8

--- Front/src/app/shared/auth.service.ts
+++ Front/src/app/shared/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   login(username: string, password: string): Observable<boolean> {
-    const loginUrl = `${environment.apiUrl}/api/login`;
+    const loginUrl = `${environment.apiUrl}/login`;
     console.log('Tentative de connexion à:', loginUrl);
     
     return this.http.post<any>(loginUrl, { login: username, password })
